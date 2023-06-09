40cabc8ef6d8f4176eaaedde227edf24fb041a80
tartan <tartanec@gmail.com>
2023-06-09 13:58:46 +0200

    database connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,4 +19,28 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	
+	fmt.Println(addr, infoLog, errorLog) //unused variable.... TODO: delete
+
+	//connect to db
+	db, err := connectToDB(*connStr)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	//close db after end of program
+	defer db.Close()
+
+	
+
+}
+
+func connectToDB(connStr string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil{
+		return nil, err
+	}
+	return db, nil
 }
