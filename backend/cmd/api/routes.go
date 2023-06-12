953cc495524071ff1319e52d660c784af2934610
tartan <tartanec@gmail.com>
2023-06-12 14:26:56 +0200

    added file server for static files

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -5,6 +5,10 @@ import "net/http"
 func (app *application) getRoutes() http.Handler {
 	router := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	router.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	router.HandleFunc("/", app.home)
 
 	return router
