4a60743935c5bc99cb9bd3e1d1c596f18af13438
tartan <tartanec@gmail.com>
2023-06-13 21:47:00 +0200

    added validators

---
+++ backend/internal/validator/validator.go
@@ -0,0 +1,40 @@
+package validator
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+func ValidUrl(value string) bool {
+	fmt.Println(value)
+	_, err := url.Parse(value)
+	return err == nil
+}
