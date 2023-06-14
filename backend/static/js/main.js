53bc653e40583f0b3ecc9e044b8d10f4f3b46013
tartan <tartanec@gmail.com>
2023-06-14 21:14:11 +0200

    completed created URL html

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -2,3 +2,12 @@ function updatePreviewUrl(e) {
   var ele = document.getElementById("PreviewURL");
   ele.innerText = `${window.location.host}/URL/${e}`;
 }
+
+function copyToCLipboard(text) {
+  navigator.clipboard.writeText(text);
+  var ele = document.getElementById("succesMessage");
+  ele.style.display = "block";
+  setTimeout(() => {
+    ele.style.display = "none";
+  }, 3000);
+}
